780dbc53890dd30e0b388928582f3f1c80a27fc8
jinfei <jinfei@supermap.com>
2026-01-21 09:13:34 +0800

    fix: update sitemap redirect to use server domain
    - Update redirect URL to use server.mcny.dpdns.org
    - This ensures sitemap.xml on client domain redirects to server's sitemap endpoint
    - Fixes cross-domain routing issue

--- client/functions/[[path]].js
+++ client/functions/[[path]].js
@@ -2,7 +2,8 @@ export async function onRequest(context) {
   const url = new URL(context.request.url);
   
   if (url.pathname === '/sitemap.xml') {
-    const sitemapUrl = `${url.origin}/seo/sitemap.xml`;
+    // Redirect to server's sitemap endpoint
+    const sitemapUrl = `https://server.mcny.dpdns.org/seo/sitemap.xml`;
     return Response.redirect(sitemapUrl, 301);
   }
   
